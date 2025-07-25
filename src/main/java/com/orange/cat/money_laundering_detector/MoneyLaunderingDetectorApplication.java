22ab25b80c047b410101a898f58a946e36b60e47
angie-xiong <angie.xiong0627@gmail.com>
2025-07-25 22:53:17 +0100

    Initial App Check-in  (#2)

---
+++ src/main/java/com/orange/cat/money_laundering_detector/MoneyLaunderingDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.orange.cat.money_laundering_detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyLaunderingDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyLaunderingDetectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orange/cat/money_laundering_detector/MoneyLaunderingDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.cat.money_laundering_detector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyLaunderingDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
