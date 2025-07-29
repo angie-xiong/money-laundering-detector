0c44aa731996df5f34b901bc6226529da93c370a
Angie Xiong <angie.xiong0627@gmail.com>
2025-07-29 20:59:11 +0100

    Integrate with code linting tools

--- src/main/java/com/orange/cat/money_laundering_detector/MoneyLaunderingDetectorApplication.java
+++ src/main/java/com/orange/cat/money_laundering_detector/MoneyLaunderingDetectorApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoneyLaunderingDetectorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoneyLaunderingDetectorApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyLaunderingDetectorApplication.class, args);
+    }
 }
