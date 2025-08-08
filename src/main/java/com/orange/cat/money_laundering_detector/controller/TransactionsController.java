b0811efab7505a0dba8feb83a4e10b05906fa14f
Angie Xiong <angie.xiong0627@gmail.com>
2025-08-08 21:59:23 +0100

    format code

--- src/main/java/com/orange/cat/money_laundering_detector/controller/TransactionsController.java
+++ src/main/java/com/orange/cat/money_laundering_detector/controller/TransactionsController.java
@@ -2,14 +2,13 @@ package com.orange.cat.money_laundering_detector.controller;
 
 import com.orange.cat.money_laundering_detector.domain.Transaction;
 import com.orange.cat.money_laundering_detector.service.TransactionsService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
