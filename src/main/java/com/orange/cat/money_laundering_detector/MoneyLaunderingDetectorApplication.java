39b822a1aaa3fd928ff75290543f58c2c8ab6e71
Angie Xiong <angie.xiong0627@gmail.com>
2025-08-14 14:58:34 +0100

    add ci workflow

--- src/main/java/com/orange/cat/money_laundering_detector/MoneyLaunderingDetectorApplication.java
+++ src/main/java/com/orange/cat/money_laundering_detector/MoneyLaunderingDetectorApplication.java
@@ -3,7 +3,7 @@ package com.orange.cat.money_laundering_detector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.orange.cat.money_laundering_detector")
+@SpringBootApplication
 public class MoneyLaunderingDetectorApplication {
 
     public static void main(String[] args) {
